d1dcc8e16cd73bf50568eef6d865c3d7a9369ce5
buschju <busch@dbs.ifi.lmu.de>
2020-09-27 20:23:56 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sscn',
+      version='0.1',
+      description='Learning Self-Expression Metrics for Scalable and Inductive Subspace Clustering',
+      author='Julian Busch',
+      author_email='busch@dbs.ifi.lmu.de',
+      packages=['sscn'],
+      )
