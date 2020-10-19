8c8bfe68643c236bb71eaffbf518a83fd03dbe45
buschju <busch@dbs.ifi.lmu.de>
2020-10-19 14:04:43 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='sscn',
-      version='0.1',
-      description='Learning Self-Expression Metrics for Scalable and Inductive Subspace Clustering',
-      author='Julian Busch',
-      author_email='busch@dbs.ifi.lmu.de',
-      packages=['sscn'],
-      )
